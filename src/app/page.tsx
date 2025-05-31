8a50c1486ae8180a333e045e393abb560c1ed740
Diego Alessandro <zentoo31@gmail.com>
2025-05-31 14:52:11 -0500

    feat: update homepage to include title and description for DocMorph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,10 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <div>
+          <h1 className="font-semibold text-4xl">DocMorph</h1>
+        </div>
+        <p>A new way to</p>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
