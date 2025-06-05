c747138485ab787730d5741473623990f6e5feca
Diego Pineda <zentoo31@gmail.com>
2025-06-05 18:46:29 -0500

    feat: add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,110 +1,73 @@
-import Image from "next/image";
-import { MorphingText } from "@/components/magicui/morphing-text";
-
-
-const texts = [
-  "Convert your files effortlessly",
-  "Transform documents with ease",
-  "Your files, our magic",
-  "File conversion made simple",
-  "Effortless file transformations",
-];
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div>
-          <h1 className="font-semibold text-4xl">DocMorph</h1>
-        </div>
-        <p>A new way to convert your files</p>
-        <MorphingText texts={texts} className="text-sm"/>
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+        <header className='flex flex-row items-start justify-between w-full mb-8'>
+          <div className="flex flex-row items-center justify-center">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={"/doc-morph-logo.svg"}
+              alt="DocMorph Logo"
+              width={50}
+              height={50}
+              className="rounded-full"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <h1 className="text-4xl font-bold ml-4">DocMorph</h1>
+          </div>
+
+          <div className='flex flex-row items-center justify-center'>
+            <nav>
+              <ul className="flex space-x-4">
+                <li>
+                  <a href="/" className="text-gray-600 hover:underline">Features</a>
+                </li>
+                <li>
+                  <a href="/about" className="text-gray-600 hover:underline">Pricing</a>
+                </li>
+                <li>
+                  <a href="/contact" className="text-gray-600 hover:underline">FAQ</a>
+                </li>
+              </ul>
+            </nav>
+            <div className='flex flex-row items-center justify-center ml-8'>
+              <button className='ml-8 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors'>
+                Login
+              </button>
+              <button className='ml-4 px-4 py-2 bg-white-600 text-black rounded transition-colors border border-gray-300 hover:bg-gray-100'>
+                Sign Up
+              </button>
+            </div>
+
+          </div>
+        </header>
+
+        <main className='flex flex-col space-y-5 items-center'>
+          <div className='flex flex-row items-center justify-center'>
+            <h1 className='text-6xl font-bold'>
+              Convert Any File 
+              <span className='text-blue-500'> Quicly & Securely</span>  
+            </h1>
+          </div>
+          <p className='text-gray-400 text-xl text-center max-w-2xl'>
+            The fastest, most secure way to convert your files online. No downloads required, no quality loss.
+          </p>
+
+          <div className='flex flex-col items-center bg-gray-50 border-dashed border-gray-300 border rounded-lg p-8 max-w-2xl space-y-2'>
+              <h2 className='text-2xl font-semibold'>Convert Your Files</h2>
+              <p className='text-gray-400 mb-4'>
+                Drag and drop your files here, or click to browse your device.
+              </p>
+              <button className='px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors'>
+                Upload your files
+                <input type="file" className='border border-gray-400 p-2 rounded w-full'  />
+              </button>
+              <p className='text-gray-400 mb-4'>
+                  Supported formats: PDF, DOCX, TXT, JPG, PNG, and more.
+              </p>
+
+          </div>
+
+        </main>
     </div>
   );
 }
